5d41e27e883b4be4272688e10d44c5e2aced2bf0
glebmillenium <GodBlessYa95@gmail.com>
2017-09-15 12:07:15 +0800

    Добавлен JS файл для дальнейшей обработки логина и пароля

---
+++ tpl/templates/index.tpl
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Информационный портал ЕЭИСЦ</title>
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/animate.css">
+		<link rel="stylesheet" href="css/styles.css">
+		<script type="text/javascript" src="process.js"> </script>
+	<body>
+
+
+		<div class="vladmaxi-top">
+			<a>Вход в информационный портал ЕЭИСЦ</a>
+			</span>
+		<div class="clr"></div>
+		</div>
+
+		<div id="container">
+			<form>
+				<label for="name">Логин:</label>
+				<input type="name">
+				<label for="username">Пароль:</label>
+				<input type="password"><br>
+						<p><a href="#">Забыли пароль?</a></p>
+				<div id="lower">
+					<input type="checkbox"><label class="check" for="checkbox">Запомнить меня</label>
+					<input type="submit" value="Войти" onclick="localhost/ARM/information_portal" >
+				</div>
+			</form>
+		</div>
+	</body>
+</html>
+	
+	
+	
+	
+	
+		
+	
---
+++ tpl/templates_c/b84a907cd7978525b154386bf6ec2f1be934ea32_0.file.index.tpl.php
@@ -0,0 +1,64 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-09-15 05:55:49
+  from "C:\xampp\htdocs\ARM\tpl\templates\index.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_59bb4f452d1cb3_50310832',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'b84a907cd7978525b154386bf6ec2f1be934ea32' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\ARM\\tpl\\templates\\index.tpl',
+      1 => 1505447741,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_59bb4f452d1cb3_50310832 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Информационный портал ЕЭИСЦ</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/styles.css">
+<body>
+
+
+    <div class="vladmaxi-top">
+        <a>Вход в информационный портал ЕЭИСЦ</a>
+        </span>
+    <div class="clr"></div>
+    </div>
+
+	<div id="container">
+		<form>
+			<label for="name">Логин:</label>
+			<input type="name">
+			<label for="username">Пароль:</label>
+			<input type="password"><br>
+					<p><a href="#">Забыли пароль?</a></p>
+			<div id="lower">
+				<input type="checkbox"><label class="check" for="checkbox">Запомнить меня</label>
+				<input type="submit" value="Войти" onclick="localhost/ARM/information_portal" >
+			</div>
+		</form>
+	</div>
+</body>
+</html>
+	
+	
+	
+	
+	
+		
+	<?php }
+}

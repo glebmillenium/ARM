2b841ee9569bd2b05fb14bffd7d5327a8753d41d
glebmillenium <GodBlessYa95@gmail.com>
2017-09-20 13:13:43 +0800

    Исправлена функциональность по сбору каталога в дерево

--- information_portal/index.php
+++ information_portal/index.php
@@ -1,8 +1,11 @@
 <?php
-	require_once("../smarty_connect.php");
-	require_once("../php/working_with_directories.php");
-	$smarty = getSmarty(array('panel'));
-	
-	$ar = WorkingWithDirectories::getArrayFiles("C:\project\\");
-	//print_r($ar);
+    require_once("../smarty_connect.php");
+    require_once("../php/working_with_directories.php");
+    $smarty = getSmarty(array('panel'));
+
+    $ierarchicalTreeFromCatalog = 
+            WorkingWithDirectories::getArrayFiles("C:\project\\");
+    $smarty->assign('ierarchicalTreeFromCatalog', $ierarchicalTreeFromCatalog);
+    
+//print_r($ar);
 ?>
--- php/working_with_directories.php
+++ php/working_with_directories.php
@@ -1,7 +1,8 @@
 <?php
+    require_once("not_standart_function.php");
     /**
      * @name WorkingWithDirectories
-     * @Статический класс для работы с корневыми 
+     *   Статический класс для работы с корневыми 
      * каталогами.
      *
      * PHP version 5
@@ -16,7 +17,7 @@
         /**
          * getArrayFiles
          * 
-         * Рекурсивный обход каталога для дальнейшей записи
+         *   Рекурсивный обход каталога для дальнейшей записи
          * всех найденных файлов в отдельные пути (массивы)
          *
          * @param string $pathDir
@@ -41,38 +42,33 @@
             {
                 $relativePath = substr($unknownPath->getPathname(), 
                         $numberCutPosition);
-                array_merge($arrayPath, self::reorganizationArray($relativePath, 
+                $arrayPath = array_merge_recursive($arrayPath, 
+                        self::reorganizationArray($relativePath, 
                         ($unknownPath->isFile() ? true : false))
                     );
 
             }
             return $arrayPath;
         } //end getArrayFiles()
-
-        public static function getRelativePath()
-        {
-            
-        }
         
         /**
          * reorganizationArray
          * 
-         * Получение ассоциативного вложенного массива с ключами, 
+         *   Получение ассоциативного вложенного массива с ключами, 
          * которые состоят из каталогов /dir_1/dir_2/.../dir_n/file1
          * В случае если путь не содержит файл, 
          * то функция вернет вложенный массив с пустым элементом
          * 
-         * @param type $arrayPath 
-         * @param type $pathToFile
-         * @param type $isFile
-         * @return type
+         * @param array $arrayPath 
+         * @param string $pathToFile
+         * @param bool   $isFile
+         * @return array
          */
         private static function reorganizationArray(
                 $pathToFile, $isFile)
         {
 
             $subDirs = explode('\\', $pathToFile);
-//            print_r($pathToFile);
             $subDirs = self::deleteSystemSubDirsSymbols($subDirs);
 
             $arrayPath = array();
@@ -84,24 +80,23 @@
             $changeArray = &$arrayPath;
             foreach($subDirs as $key => $dir)
             {
-                $changeArray[$dir] = NULL;
+                $changeArray[$dir] = array();
                 $changeArray = &$changeArray[$dir];
             }
             if($isFile)
             {
                 $changeArray = $nameFile;
             }
-            print_r($pathToFile);
-            echo "<br/>";
-            print_r($arrayPath);
-            echo "<br/>";
             return $arrayPath;
         } //end reorganizationArray()
         
         /**
+         * deleteSystemSubDirsSymbols
          * 
-         * @param type $array
-         * @return type
+         * Удаляет системные символы: ".", ".." из массива
+         *  
+         * @param array $array Массив на обработку
+         * @return array Обработанный массив
          */
         private static function deleteSystemSubDirsSymbols($array)
         {

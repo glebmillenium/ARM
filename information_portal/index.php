5eb5c11a2666daf726c4f8f064eea2df0ee563a1
glebmillenium <GodBlessYa95@gmail.com>
2017-09-20 09:57:08 +0800

    Добавлен класс для получения дерева каталогов в виде вложенного ассоциативного массива

--- information_portal/index.php
+++ information_portal/index.php
@@ -3,7 +3,6 @@
 	require_once("../php/working_with_directories.php");
 	$smarty = getSmarty(array('panel'));
 	
-	$ar = new WorkingWithDirectories();
-	$ar->getArrayFiles("C:\project");
-	print_r($ar);
+	$ar = WorkingWithDirectories::getArrayFiles("C:\project\\");
+	//print_r($ar);
 ?>
--- php/working_with_directories.php
+++ php/working_with_directories.php
@@ -1,40 +1,111 @@
 <?php
-	/**
-	 * WorkingWithDirectories
-	 * Статический класс для работы с корневыми 
-	 * каталогами.
-	 *
-	 * PHP version 5
-	 *
-	 * @category  PHP
-	 * @package   none
-	 * @author    Gleb An <An_gv@es.irkutskenergo.ru>
-	 * @date      19.09.2017
-	 */
-	class WorkingWithDirectories{
-		
-		/**
-		 * getArrayFiles
-		 * Рекурсивный обход каталога для дальнейшей записи
-		 * всех найденных файлов в отдельные пути (массивы)
-		 *
-		 * @param string $pathDir
-		 *
-		 * @return array
-		 */
-		public static function getArrayFiles($pathDir)
-		{
-			$array_path = array();
-			$dir = new RecursiveDirectoryIterator($pathDir);
-			foreach (new RecursiveIteratorIterator($dir) as $val)
-			{
-				if ($val->isFile() === true)
-				{
-					$array_path[] = $val->getPathname();
-				}
-			}
-			return $array_path;
-		} //end getArrayFiles()
-		
-	}
+    /**
+     * @name WorkingWithDirectories
+     * @Статический класс для работы с корневыми 
+     * каталогами.
+     *
+     * PHP version 5
+     *
+     * @category  PHP
+     * @package   none
+     * @author    Gleb An <An_gv@es.irkutskenergo.ru>
+     * @date      19.09.2017
+     */
+    class WorkingWithDirectories{
+
+        /**
+         * getArrayFiles
+         * 
+         * Рекурсивный обход каталога для дальнейшей записи
+         * всех найденных файлов в отдельные пути (массивы)
+         *
+         * @param string $pathDir
+         *
+         * @return array
+         */
+        public static function getArrayFiles($pathDir)
+        {
+            $numberCutPosition = strripos($pathDir, '\\');
+            if($numberCutPosition)
+            {
+                $numberCutPosition++;
+            }
+            
+            $arrayPath = array();
+            $arrayContainsAllInnerStructureCurrentDirectory = 
+                    new RecursiveDirectoryIterator($pathDir);
+
+            foreach (new RecursiveIteratorIterator(
+                    $arrayContainsAllInnerStructureCurrentDirectory) 
+                    as $unknownPath)
+            {
+                $relativePath = substr($unknownPath->getPathname(), 
+                        $numberCutPosition);
+                array_merge($arrayPath, self::reorganizationArray($relativePath, 
+                        ($unknownPath->isFile() ? true : false))
+                    );
+
+            }
+            return $arrayPath;
+        } //end getArrayFiles()
+
+        public static function getRelativePath()
+        {
+            
+        }
+        
+        /**
+         * reorganizationArray
+         * 
+         * Получение ассоциативного вложенного массива с ключами, 
+         * которые состоят из каталогов /dir_1/dir_2/.../dir_n/file1
+         * В случае если путь не содержит файл, 
+         * то функция вернет вложенный массив с пустым элементом
+         * 
+         * @param type $arrayPath 
+         * @param type $pathToFile
+         * @param type $isFile
+         * @return type
+         */
+        private static function reorganizationArray(
+                $pathToFile, $isFile)
+        {
+
+            $subDirs = explode('\\', $pathToFile);
+//            print_r($pathToFile);
+            $subDirs = self::deleteSystemSubDirsSymbols($subDirs);
+
+            $arrayPath = array();
+
+            if ($isFile)
+            {
+                $nameFile = array_pop($subDirs);
+            }
+            $changeArray = &$arrayPath;
+            foreach($subDirs as $key => $dir)
+            {
+                $changeArray[$dir] = NULL;
+                $changeArray = &$changeArray[$dir];
+            }
+            if($isFile)
+            {
+                $changeArray = $nameFile;
+            }
+            print_r($pathToFile);
+            echo "<br/>";
+            print_r($arrayPath);
+            echo "<br/>";
+            return $arrayPath;
+        } //end reorganizationArray()
+        
+        /**
+         * 
+         * @param type $array
+         * @return type
+         */
+        private static function deleteSystemSubDirsSymbols($array)
+        {
+            return array_diff($array, array(".", ".."));
+        } // end deleteSystemSubDirsSymbols()
+    }
 ?>
